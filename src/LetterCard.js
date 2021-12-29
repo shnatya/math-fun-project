1c0288ac91eb703faa23f6ca876ee5df91f816c2
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-28 22:23:03 -0800

    Work on math equation

--- src/LetterCard.js
+++ src/LetterCard.js
@@ -3,7 +3,8 @@ import React from "react";
 function LetterCard({letter}) {
     return (
         <div>
-            
+            <h1>{letter}</h1>
+            <h2>{Math.floor(Math.random()*1000)} - {Math.floor(Math.random()*100)}</h2>
         </div>
     )
 }
