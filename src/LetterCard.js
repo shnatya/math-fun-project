7043d2ce52467c6b0ce6d657f4060100b40c3acd
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-29 09:30:40 -0800

    Render max number first then min in equations

--- src/LetterCard.js
+++ src/LetterCard.js
@@ -3,12 +3,19 @@ import React from "react";
 function LetterCard({letter}) {
     const min = 100;
     const max = 999; 
+
+    const number1 = Math.floor(Math.random()*(max-min)+min)
+    const number2 = Math.floor(Math.random()*(max-min)+min)
+   
+    const maxNumber = Math.max(number1, number2)
+    const minNumber = Math.min(number1, number2)
+
     return (
-        <div class ='border'>
+        <div className='margin border'>
             <form >
                 <h1>{letter}</h1>
-                <h2>{Math.floor(Math.random()*(max-min)+min)} - {Math.floor(Math.random()*min)}</h2>
-                <input class='border-input' type="text" name="answer" placeholder="your answer"/>
+                <h2 className='margin'>{maxNumber} - {minNumber}</h2>
+                <input className='margin input' type="text" name="answer" size='10' placeholder="your answer"/>
             </form>
         </div>
     )
