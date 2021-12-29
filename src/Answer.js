f1b9941aadc923328ad892536c52b7620a421aba
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-28 21:49:45 -0800

    Add LetterCard file

--- src/Answer.js
+++ src/Answer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import LetterCard from "./LetterCard"
 
 function Answer({answer}) {
     const arrayOfLetters = answer.split("")
-    console.log(arrayOfLetters)
+    
     return (
         <div>
-            {}
+            {arrayOfLetters.map((letter, index) => <LetterCard letter={letter} key={index}/>)}
         </div>
     )
 }
---
+++ src/LetterCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LetterCard({letter}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LetterCard;
