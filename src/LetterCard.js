d964990d5cdc4a9457e229bff915fdfb400f00c9
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-28 22:33:03 -0800

    Add input field

--- src/LetterCard.js
+++ src/LetterCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function LetterCard({letter}) {
+    const min = 100;
+    const max = 999; 
     return (
         <div>
-            <h1>{letter}</h1>
-            <h2>{Math.floor(Math.random()*1000)} - {Math.floor(Math.random()*100)}</h2>
+            <form >
+                <h1>{letter}</h1>
+                <h2>{Math.floor(Math.random()*(max-min)+min)} - {Math.floor(Math.random()*min)}</h2>
+                <input type="text" name="answer" placeholder="your answer"/>
+            </form>
         </div>
     )
 }
