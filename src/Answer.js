e4edf201ef226673019e333f5b9052c1e18ba8d7
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-28 21:41:04 -0800

    Console log array of letters

--- src/Answer.js
+++ src/Answer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Answer({answer}) {
+    const arrayOfLetters = answer.split("")
+    console.log(arrayOfLetters)
     return (
         <div>
-            
+            {}
         </div>
     )
 }
