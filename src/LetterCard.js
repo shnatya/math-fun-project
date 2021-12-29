94d6b29eb60b766434f0a22533b6d54e2eb96e2e
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-29 08:13:32 -0800

    Add class for input

--- src/LetterCard.js
+++ src/LetterCard.js
@@ -4,11 +4,11 @@ function LetterCard({letter}) {
     const min = 100;
     const max = 999; 
     return (
-        <div class ='border border-round padding'>
+        <div class ='border'>
             <form >
                 <h1>{letter}</h1>
                 <h2>{Math.floor(Math.random()*(max-min)+min)} - {Math.floor(Math.random()*min)}</h2>
-                <input type="text" name="answer" placeholder="your answer"/>
+                <input class='border-input' type="text" name="answer" placeholder="your answer"/>
             </form>
         </div>
     )
