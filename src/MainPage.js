e6548332977ea945e16953e2418bf8717ad282a9
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-28 18:44:37 -0800

    Add Header, MainPage, NewJokeForm files

---
+++ src/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NewJokeForm from "./NewJokeForm";
+
+function MainPage() {
+    return (
+        <main>
+            <NewJokeForm />
+            
+        </main>
+    )
+}
+
+export default MainPage;
