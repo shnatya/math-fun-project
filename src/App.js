1e84853419d949fbef4f4772d6caa69fe294f5dc
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-29 10:40:53 -0800

    Work on routing, add BrowserRouter, route libraries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './Header'
 import MainPage from './MainPage';
+import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <Switch>
+        <Route exact path='/'>
+          <MainPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <h1>
-            Fun Math 😂
+            Fun Math 😻
         </h1>
     )
 }
