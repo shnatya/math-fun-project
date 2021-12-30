1077cf4d8aef8862cc834c5601c3bea2c08b34fb
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-30 12:55:54 -0800

    Shuffle letters in anser. Use Fisher-Yates algorithm

--- src/Answer.js
+++ src/Answer.js
@@ -3,7 +3,17 @@ import LetterCard from "./LetterCard"
 
 function Answer({answerOfJoke, settings}) {
     const arrayOfLetters = answerOfJoke.split("")
+    const arrayLength = arrayOfLetters.length;
     
+    for(let i = arrayLength-1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+
+        let temperateVar = arrayOfLetters[i];
+        arrayOfLetters[i] = arrayOfLetters[j];
+        arrayOfLetters[j] = temperateVar;
+        console.log(arrayOfLetters)
+     }
+     
     return (
         <div className='flex-container'>
             {arrayOfLetters.map((letter, index) => <LetterCard letter={letter} key={index} settings={settings} />)}
